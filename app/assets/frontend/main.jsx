bd18355be9fc26bf01ab053978b2eb6b856d780d
Sergey Timofeev <ug.tiser@gmail.com>
2016-02-22 20:55:01 +0200

    Add user, profile and tweet backend

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -23,10 +23,10 @@ export class Main extends React.Component {
 }
 
 let documentReady = () => {
-  ReactDOM.render(
-    <Main />,
-      document.getElementById('react')
-  );
+  let reactNode = document.getElementById('react')
+  if (reactNode) {
+    ReactDOM.render(<Main />, reactNode);
+  }
 }
 
 $(documentReady);
--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -109,7 +109,10 @@ var ReactApp =
 	}(React.Component);
 	
 	var documentReady = function documentReady() {
-	  ReactDOM.render(React.createElement(Main, null), document.getElementById('react'));
+	  var reactNode = document.getElementById('react');
+	  if (reactNode) {
+	    ReactDOM.render(React.createElement(Main, null), reactNode);
+	  }
 	};
 	
 	$(documentReady);
