140d87bf69d2dc207f14a7deaac99f1ad4de27d1
Sergey Timofeev <ug.tiser@gmail.com>
2016-02-25 13:25:56 +0200

    Add React as node module, add followers and who to follow

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,34 +1,15 @@
-import TweetBox from "./components/TweetBox";
-import TweetList from "./components/TweetsList";
-import TweetStore from "./stores/TweetStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from "./components/Index";
+import Follow from "./components/Follow";
 
-import TweetActions from "./actions/TweetActions";
-TweetActions.getAllTweets();
+import { Router, Route, Link } from 'react-router'
 
-let getAppState = () => {
-  return { tweetsList: TweetStore.getAll() };
-}
-
-export class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = getAppState();
-    this._onChange = this._onChange.bind(this);
-  }
-  componentDidMount() {
-    TweetStore.addChangeListener(this._onChange);
-  }
-  componentWillUnmount() {
-    TweetStore.removeChangeListener(this._onChange);
-  }
-  _onChange() {
-    this.setState(getAppState());
-  }
+class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <TweetBox />
-        <TweetList tweets={this.state.tweetsList}/>
+      <div>
+        {this.props.children}
       </div>
     );
   }
@@ -37,7 +18,14 @@ export class Main extends React.Component {
 let documentReady = () => {
   let reactNode = document.getElementById('react')
   if (reactNode) {
-    ReactDOM.render(<Main />, reactNode);
+    ReactDOM.render(
+      <Router>
+        <Route component={App}>
+          <Route path="/" component={Index} />
+          <Route path="/follow" component={Follow} />
+        </Route>
+      </Router>
+      , reactNode);
   }
 }
 
